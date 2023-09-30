52fabb3777b2b6578e7f9255ddee6bc78e5857c2
aniket <aniketpandey651@gmail.com>
2023-09-30 08:18:09 +0530

    merge sort complete

--- js-files/merge.js
+++ js-files/merge.js
@@ -1,43 +1,71 @@
 let mergeDelay
 
+let mergedArr = [];
 
-const merge = (bars,l,r)=>{
-
-    let pt1 = l,pt2 = l+ (r-l)/2 + 1;
+const merge = async(l,r)=>{
+    let mid = l + Math.floor((r-l)/2);
+    let pt1 = l,pt2 = mid + 1;
     let arr = []
-    while((pt1 <= l+ (r-l)/2)  && (pt2<=r)){
-        if(bars[pt1].clientHeight <= bars[pt2].clientHeight){
-            arr.push(bars[pt1].clientHeight);
-            pt1++;
+
+    while(pt1 <= mid  && (pt2<=r)){
+        if(mergedArr[pt1] <= mergedArr[pt2]){
+            arr.push(mergedArr[pt1]);
+            pt1 = pt1 + 1;
         }
         else{
-            arr.push(bars[pt2].clientHeight);
-            pt2++;
+            arr.push(mergedArr[pt2]);
+            pt2 = pt2 + 1;
         }
     }
-    while(pt1<=l+ (r-l)/2){
-        arr.push(bars[pt1].clientHeight);
-        pt1++;
+    while(pt1<=mid){
+        arr.push(mergedArr[pt1]);
+        pt1 = pt1 + 1;
     }
     while(pt2<=r){
-        arr.push(bars[pt2].clientHeight);
-        pt2++;
+        arr.push(mergedArr[pt2]);
+        pt2 = pt2 + 1;
     }
     for(let i = l;i<=r;i++){
-        let s = (arr[i]).toString() + "px";
-        bar.style.height = s 
+        mergedArr[i] = arr[i-l];
+    }
+
+    return;
+}
+
+const changeHeight = (bars,l,r)=>{
+    for(let i = l;i<=r;i++){
+        bars[i].style.height = mergedArr[i]+"px";
     }
     return;
 }
 
+const changeColor1 = async(bars,l,r)=>{
+    for(let i = l;i<=r;i++){
+        bars[i].style.backgroundColor = "yellow";
+    }
+    await new Promise(resolve => setTimeout(resolve, mergeDelay));
+    return;
+}
+
+const changeColor2 = (bars,l,r)=>{
+    for(let i = l;i<=r;i++){
+        bars[i].style.backgroundColor = "white";
+    }
+    return;
+}
 
 const mergeSort = async(bars,l,r)=>{
 
-    if(l>r) return;
-
-    await mergeSort(bars,l,l + (r-l)/2);
-    await mergeSort(bars,l+(r-l)/2+1,r);
-    await merge(bars,l,r);
+    if(l>=r) return;
+    // await changeColor1(bars,l,r);
+    let md = l + Math.floor((r-l)/2);
+    await mergeSort(bars,l,md);
+    await mergeSort(bars,md+1,r);
+    await changeColor1(bars,l,r);
+    await merge(l,r);
+    await changeHeight(bars,l,r);
+    await changeColor2(bars,l,r);
+    await new Promise(resolve => setTimeout(resolve, mergeDelay));
     return;
 }
 
@@ -57,7 +85,9 @@ const startMergeSorting = async()=>{
             mergeDelay = i.value
         }
     }
-
+    for(let i = 0;i<bars.length;i++){
+        mergedArr.push(bars[i].clientHeight);
+    }
     await mergeSort(bars,0,bars.length -1);
     for(const i of disablenow){
         i.removeAttribute("disabled")
